215dab1fc4106bd497a644ab825b778d08fe12cc
KyungHoon <nephogram@gmail.com>
2015-03-01 01:45:35 +0900

    Address, Blockchain Status, Transaction API Test Code 개발

--- src/main/java/io/cloudwallet/coinstack/CloudWalletBackEndAdaptor.java
+++ src/main/java/io/cloudwallet/coinstack/CloudWalletBackEndAdaptor.java
@@ -4,8 +4,12 @@
 package io.cloudwallet.coinstack;
 
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.TimeZone;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
@@ -68,9 +72,69 @@ public class CloudWalletBackEndAdaptor extends MockCoinStackAdaptor {
 		return null;
 	}
 
+	private static DateFormat dateFormat = new SimpleDateFormat(
+			"yyyy-MM-dd'T'HH:mm:ss.SSS");
+	static {
+		dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+	}
+
 	@Override
 	public Transaction getTransaction(String transactionId) throws IOException {
-		return null;
+		HttpGet httpGet = new HttpGet(
+				"http://search.cloudwallet.io/api/transactions/"
+						+ transactionId);
+		HttpResponse res = httpClient.execute(httpGet);
+		String resJsonString = EntityUtils.toString(res.getEntity());
+		JSONObject resJson;
+
+		try {
+			String[] blockIds;
+			Input[] inputs;
+			Output[] outputs;
+
+			resJson = new JSONObject(resJsonString);
+
+			JSONArray transactionBlockIds = resJson.getJSONArray("blockhash");
+			blockIds = new String[transactionBlockIds.length()];
+			for (int i = 0; i < transactionBlockIds.length(); i++) {
+				blockIds[i] = transactionBlockIds.getJSONObject(i).getString(
+						"blockhash");
+
+			}
+
+			JSONArray transactionInputs = resJson.getJSONArray("inputs");
+			inputs = new Input[transactionInputs.length()];
+			for (int i = 0; i < transactionInputs.length(); i++) {
+				inputs[i] = new Input(transactionInputs.getJSONObject(i)
+						.getInt("index"), transactionInputs.getJSONObject(i)
+						.getString("address"), transactionInputs.getJSONObject(
+						i).getString("hash"), transactionInputs
+						.getJSONObject(i).getLong("value"));
+
+			}
+
+			JSONArray transactionOutputs = resJson.getJSONArray("outputs");
+			outputs = new Output[transactionOutputs.length()];
+			for (int i = 0; i < transactionOutputs.length(); i++) {
+				JSONObject transactionOutput = transactionOutputs
+						.getJSONObject(i);
+				outputs[i] = new Output(transactionId,
+						transactionOutput.getInt("index"),
+						transactionOutput.getString("address"),
+						transactionOutput.getBoolean("used"),
+						transactionOutput.getLong("value"),
+						transactionOutput.getString("script"));
+
+			}
+
+			return new Transaction(resJson.getString("_id"), blockIds,
+					dateFormat.parse(resJson.getString("timestamp")),
+					resJson.getBoolean("coinbase"), inputs, outputs);
+		} catch (JSONException e) {
+			throw new IOException("Parsing response failed", e);
+		} catch (ParseException e) {
+			throw new IOException("Parsing response failed", e);
+		}
 	}
 
 	@Override
